714cf223a248bf9e1b300280efbb1db1b0283c9b
haicauvn <haidv245@gmail.com>
2022-12-11 22:14:05 +0700

    Remove default item in cart

--- src/app/product-cart/service/product-cart.service.ts
+++ src/app/product-cart/service/product-cart.service.ts
@@ -6,16 +6,7 @@ import { ProductListModal } from 'src/app/product-list/product-list.modal';
 })
 export class ProductCartService {
 
-  carts: ProductListModal[] = [
-    {
-      "id": 1,
-      "name": "Book",
-      "price": 9.99,
-      "url": "https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
-      "description": "You can read it!",
-      "amount": 1
-    }
-  ];
+  carts: ProductListModal[] = [];
 
   constructor() {}
 
